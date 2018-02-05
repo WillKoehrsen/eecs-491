68574b26fd1e50f874d43bc7d0370e871f5227ee
Will Koehrsen <wjk68@case.edu>
2018-02-05 13:19:55 -0500

    Completed Report

--- assign/assign_1/mycfg.py
+++
@@ -1,8 +0,0 @@
-c = get_config()
-
-c.TemplateExporter.exclude_input_prompt = True
-c.TemplateExporter.exclude_output_prompt = True
-c.NbConvertApp.notebooks = ['assign_1.ipynb']
-c.NbConvertApp.export_format = 'pdf'
-
-
